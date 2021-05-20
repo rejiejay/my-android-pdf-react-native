027ce5df8d4132d11e52132350ce4028e4174a21
rejiejay <454766952@qq.com>
2021-05-20 22:39:32 +0800

    update app

---
+++ README.md
@@ -0,0 +1,13 @@
+# 项目初始化
+
+npx react-native init AndroidPdfReactNative
+
+## react-native-fs
+https://github.com/itinance/react-native-fs
+
+cnpm install react-native-fs --save -d
+
+
+## gradlew clean
+
+cd android && gradlew clean && cd ..
---
+++ app/view/entry.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import {
+    StyleSheet,
+    Text,
+    View,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+});
+
+const Entry = () => {
+    return (
+        <View>
+            <Text>Hello World</Text>
+        </View>
+    );
+}
+
+export default Entry;
